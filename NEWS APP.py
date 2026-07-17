a03239f1f8d2084386907eb62b35520d6ca617a8
PranjalxDEV <166709557+PRANJALxDEVxAI@users.noreply.github.com>
2026-07-17 00:49:40 -0700

    Add files via upload

---
+++ NEWS APP.py
@@ -0,0 +1,21 @@
+import requests
+def get_news(topic):
+    API_KEY = "99ce8adb6e24442cb8303b9faa1a5fc1"
+    url = f"https://newsapi.org/v2/everything?q={topic}&language=en&sortBy=publishedAt&apiKey={API_KEY}"
+    response= requests.get(url)
+    data = response.json()
+    if data["status"] != "ok" :
+        print("Error:" , data.get("message"))
+        return
+    
+    print(f"\nLatest News on {topic}\n")
+
+    for i ,article in enumerate(data["articles"], 1):
+        print(f"{i}. {article['title']}")
+        print(article["description"])
+        print(article["url"])
+        print("-"*50)
+        
+
+topic = input("Topic: ")
+get_news(topic)
